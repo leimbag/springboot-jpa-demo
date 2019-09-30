496f5f62e177a164f5287581077f174a3289eb72
leiming <leiming@egcchain.com>
2019-09-30 11:22:17 +0800

    增加druid配置

--- src/main/java/com/leimbag/jpa/demo/config/JpaConfig.java
+++ src/main/java/com/leimbag/jpa/demo/config/JpaConfig.java
@@ -1,15 +1,91 @@
 package com.leimbag.jpa.demo.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import com.leimbag.jpa.demo.repository.BaseRepositoryFactoryBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DatabaseDriver;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 /**
  * @author leimbag
  */
 @Configuration
-@EnableJpaRepositories(repositoryFactoryBeanClass = BaseRepositoryFactoryBean.class, basePackages ="com.leimbag.jpa.demo.repository")
+@EnableJpaRepositories(repositoryFactoryBeanClass = BaseRepositoryFactoryBean.class, basePackages = "com.leimbag.jpa.demo.repository")
 @EnableTransactionManagement
+@ConditionalOnClass(com.alibaba.druid.pool.DruidDataSource.class)
 public class JpaConfig {
+    @SuppressWarnings("unchecked")
+    protected <T> T createDataSource(DataSourceProperties properties, Class<? extends DataSource> type) {
+        return (T) properties.initializeDataSourceBuilder().type(type).build();
+    }
+
+    /**
+     * @param properties 读入的配置
+     * @return DruidDataSource
+     */
+    @Bean
+    @ConfigurationProperties("spring.datasource.druid")
+    public DruidDataSource dataSource(DataSourceProperties properties) {
+
+        DruidDataSource dataSource = createDataSource(properties, DruidDataSource.class);
+
+        DatabaseDriver databaseDriver = DatabaseDriver.fromJdbcUrl(properties.determineUrl());
+
+        String validationQuery = databaseDriver.getValidationQuery();
+        if (validationQuery != null) {
+            dataSource.setTestOnBorrow(true);
+            dataSource.setValidationQuery(validationQuery);
+        }
+
+        return dataSource;
+    }
+
+    /**
+     * 注册一个StatViewServlet
+     */
+    @Bean
+    public ServletRegistrationBean druidStatViewServlet() {
+        //org.springframework.boot.context.embedded.ServletRegistrationBean提供类的进行注册.
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+
+        //添加初始化参数：initParams
+        //白名单：
+        servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
+        //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view this page.
+        servletRegistrationBean.addInitParameter("deny", "192.168.1.20");
+        //登录查看信息的账号密码.
+        servletRegistrationBean.addInitParameter("loginUsername", "root");
+        servletRegistrationBean.addInitParameter("loginPassword", "123456");
+        //是否能够重置数据.
+        servletRegistrationBean.addInitParameter("resetEnable", "false");
+        // 禁用HTML页面上的“Reset All”功能
+        return servletRegistrationBean;
+    }
+
+    /**
+     * 注册一个：filterRegistrationBean
+     * 过滤拦截
+     */
+    @Bean
+    public FilterRegistrationBean druidStatFilter() {
+
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        filterRegistrationBean.setName("druidWebStatFilter");
+        //添加过滤规则.
+        filterRegistrationBean.addUrlPatterns("/*");
+        //添加忽略的格式信息.
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
 }
